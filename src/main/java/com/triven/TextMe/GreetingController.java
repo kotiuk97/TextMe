fe31b0526214ba95af2a952e3ffe36d13fb719cc
skotyuk <skotyuk@computervoice.ro>
2018-09-05 19:38:27 +0300

    Implemented message sending capability

--- src/main/java/com/triven/TextMe/GreetingController.java
+++ src/main/java/com/triven/TextMe/GreetingController.java
@@ -1,14 +1,22 @@
 package com.triven.TextMe;
 
+import com.triven.TextMe.domain.Message;
+import com.triven.TextMe.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(
@@ -21,7 +29,40 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model){
-        model.put("some", "hello, trivenchik!");
+        Iterable<Message> messages = messageRepo.findAll();
+
+        model.put("messages", messages);
         return "main";
+    }
+
+    @PostMapping
+    public String submit(
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model){
+        Message message = new Message(text, tag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
+        return "main";
+
+    }
+
+    @PostMapping("filter")
+    public String filter(
+            @RequestParam(defaultValue = "") String filter,
+            Map<String, Object> model){
+        Iterable<Message> messages;
+        if (filter != null && !filter.isEmpty()){
+            messages = messageRepo.findByTag(filter);
+        }else{
+            messages = messageRepo.findAll();
+        }
+        model.put("messages", messages);
+
+        return "main";
+
     }
 }
